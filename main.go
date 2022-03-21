83d086e77d058dddb8ecd1c5c4990db0dc934cd0
Andrea Pavoni <andrea.pavoni@prima.it>
2022-03-21 16:25:35 +0100

    remove bot pkg

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
-	"github.com/andreapavoni/ttfm_bot/bot"
+	"github.com/andreapavoni/ttfm_bot/ttfm"
+	"github.com/andreapavoni/ttfm_bot/ttfm/commands"
 )
 
 func main() {
-	b := bot.New()
+	b := ttfm.New()
+	b.AddCommand("dj", commands.DjCommand())
+	b.AddCommand("escort", commands.EscortCommand())
+	b.AddCommand("escortme", commands.EscortMeCommand())
+	b.AddCommand("autobop", commands.AutoBopCommand())
+	b.AddCommand("autodj", commands.AutoDjCommand())
+	b.AddCommand("autosnag", commands.AutoSnagCommand())
+	b.AddCommand("bop", commands.BopCommand())
+	b.AddCommand("dj", commands.DjCommand())
+	b.AddCommand("snag", commands.SnagCommand())
+	b.AddCommand("fan", commands.FanCommand())
+	b.AddCommand("unfan", commands.UnfanCommand())
+	b.AddCommand("props", commands.PropsCommand())
+	b.AddCommand("skip", commands.SkipCommand())
+	b.AddCommand("boot", commands.BootCommand())
+	b.AddCommand("padd", commands.PlaylistCreateCommand())
+	b.AddCommand("pdel", commands.PlaylistDeleteCommand())
+	b.AddCommand("pls", commands.PlaylistListCommand())
+	b.AddCommand("prm", commands.PlaylistRemoveSongCommand())
+	b.AddCommand("pc", commands.PlaylistSwitchCommand())
+	b.AddCommand("q", commands.QueueCommand())
+	b.AddCommand("qadd", commands.QueueAddCommand())
+	b.AddCommand("qrm", commands.QueueRemoveCommand())
+	b.AddCommand("cfg", commands.SetConfigCommand())
+	b.AddCommand("say", commands.SayCommand())
+	b.AddCommand("help", commands.HelpCommand())
+	b.AddCommand("r", commands.ReactionCommand())
 	b.Start()
 }
